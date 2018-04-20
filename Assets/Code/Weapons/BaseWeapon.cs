dfd4ed0732007fe2462f07fc01295382352ae9b1
ukibs <martincativiela@gmail.com>
2018-04-20 19:02:27 +0200

    Añadido control de armas configurable

---
+++ Assets/Code/Weapons/BaseWeapon.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BaseWeapon : NetworkBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform shootPoint;
+
+    private float fireRate = 0.5f;
+    private float fireCooldown = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dt = Time.deltaTime;
+
+        if (fireCooldown < fireRate) fireCooldown += dt;
+    }
+
+    //
+    public bool OrderFire()
+    {
+        if (Input.GetAxis("Fire1") != 0.0f && fireCooldown >= fireRate)
+        {
+
+            // CmdFire();
+            fireCooldown = 0.0f;
+            return true;
+        }
+        return false;
+    }
+
+    
+}
