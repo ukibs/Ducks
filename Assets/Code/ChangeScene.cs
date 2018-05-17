f9d76cf0161aa71552bc22c02d0fa1674180e81b
ukibs <martincativiela@gmail.com>
2018-05-17 17:08:53 +0200

    Añadidas escenas nuevas, con sus canvas tanto para host como para client, reorganizado todos los scripts en carpetas

---
+++ Assets/Code/ChangeScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ChangeScene : MonoBehaviour {
+
+    public NetworkManager nm;
+
+	// Use this for initialization
+	void Start () {
+        nm = FindObjectOfType<NetworkManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void ChangeToScene(string sceneToChange)
+    {
+        nm.ServerChangeScene(sceneToChange);
+    }
+}
---
+++ Assets/Code/SelectionCanvasManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SelectionCanvasManager : NetworkBehaviour {
+
+    public GameObject selectionButton;
+    public GameObject loadingText;
+
+	// Use this for initialization
+	void Start () {
+        if (isServer)
+        {
+            Debug.Log("In Server");
+            loadingText.SetActive(false);
+            selectionButton.SetActive(true);
+        }
+        else if (isClient)
+        {
+            Debug.Log("In Client");
+            selectionButton.SetActive(false);
+            loadingText.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Something's wrong");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
