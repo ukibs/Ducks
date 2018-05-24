76bad5b2bf3dbfd1ca02b0ebfea97e4ad9a164df
LAPTOP-6855M5OO\Cristina <criix_rubi@hotmail.com>
2018-05-24 16:30:48 +0200

    Añadido boton a escenario 3, el hud ya no aparece en el selector de maps

--- Assets/Code/HUD.cs
+++ Assets/Code/HUD.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 public class HUD : NetworkBehaviour {
 
@@ -12,7 +13,7 @@ public class HUD : NetworkBehaviour {
 	public Texture bombCooldown;
 
 	private PlayerController player;
-
+	private Scene currentScene;
 	// Use this for initialization
 	void Start () {
 		player = GetComponent<PlayerController> ();
@@ -20,7 +21,8 @@ public class HUD : NetworkBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		currentScene = SceneManager.GetActiveScene();
+
 	}
 
 	void NesxtScene(){
@@ -29,33 +31,39 @@ public class HUD : NetworkBehaviour {
 
 	private void OnGUI()
 	{
-		if (isLocalPlayer) {
-			// Weapon info
-			BaseWeapon weaponData = player.CurrentWeapon;
-			//Bullets
-			GUI.Label (new Rect (Screen.width * 7.8f / 10, Screen.height * 9f / 10, 150, 30), imageBullet);
-			GUI.Label (new Rect (Screen.width * 8.1f / 10, Screen.height * 9.1f / 10, 150, 30), weaponData.CurrentWeaponAmmo + "/" + weaponData.maxWeaponAmmo);
-			//Recharge weapon
-			GUI.Label (new Rect (Screen.width * 8.5f / 10, Screen.height * 9 / 10, 150, 30), imageRecharge);
-			GUI.Label (new Rect (Screen.width * 9.1f / 10, Screen.height * 9.1f / 10, 100, 20), weaponData.CurrentReserveAmmo + "/" + weaponData.maxReserveAmmo);
+		if (currentScene.name != "SelectorOfMaps") {
+		
+			if (isLocalPlayer) {
+				// Weapon info
+				BaseWeapon weaponData = player.CurrentWeapon;
+				//Bullets
+				GUI.Label (new Rect (Screen.width * 7.8f / 10, Screen.height * 9f / 10, 150, 30), imageBullet);
+				GUI.Label (new Rect (Screen.width * 8.1f / 10, Screen.height * 9.1f / 10, 150, 30), weaponData.CurrentWeaponAmmo + "/" + weaponData.maxWeaponAmmo);
+				//Recharge weapon
+				GUI.Label (new Rect (Screen.width * 8.5f / 10, Screen.height * 9 / 10, 150, 30), imageRecharge);
+				GUI.Label (new Rect (Screen.width * 9.1f / 10, Screen.height * 9.1f / 10, 100, 20), weaponData.CurrentReserveAmmo + "/" + weaponData.maxReserveAmmo);
 
-			//GUI.Label(new Rect(10, 10, 350, 20), "State: " + player.State + ", movement state: " + player.);
+				//GUI.Label(new Rect(10, 10, 350, 20), "State: " + player.State + ", movement state: " + player.);
 
-			//Score
-			GUI.Label (new Rect (Screen.width * 9 / 10, Screen.height * 0.7f / 10, 100, 20), "Score: " + player.Score);
+				//Score
+				GUI.Label (new Rect (Screen.width * 9 / 10, Screen.height * 0.7f / 10, 100, 20), "Score: " + player.Score);
 
-			//Blind screen
-			if(player.getCooldown(4) != 0)
-				GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), black);
-			for (int i = 0; i < 4; i++) 
-			{
-				if (player.getCooldown (i) != 0) 
+				//Blind screen
+				if(player.getCooldown(4) != 0)
+					GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), black);
+
+				for (int i = 0; i < 4; i++) 
 				{
-					GUI.DrawTexture (new Rect (Screen.width * i / 10, Screen.height * 9 / 10, Screen.width / 10, Screen.height * 0.75f / 10), bombCooldown, ScaleMode.StretchToFill);
+					if (player.getCooldown (i) != 0) 
+					{
+						GUI.DrawTexture (new Rect (Screen.width * i / 10, Screen.height * 9 / 10, Screen.width / 10, Screen.height * 0.75f / 10), bombCooldown, ScaleMode.StretchToFill);
+					}
+					else
+						GUI.DrawTexture (new Rect (Screen.width * i / 10, Screen.height * 9 / 10, Screen.width / 10, Screen.height * 0.75f / 10), bomb, ScaleMode.StretchToFill);
 				}
-				else
-					GUI.DrawTexture (new Rect (Screen.width * i / 10, Screen.height * 9 / 10, Screen.width / 10, Screen.height * 0.75f / 10), bomb, ScaleMode.StretchToFill);
 			}
+		
 		}
+
 	}
 }
